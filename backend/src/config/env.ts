dd139c662e8bcc31c84bc4f6ad9b107add99d07d
Shivam Rana <4826shivam@gmail.com>
2025-11-22 19:56:51 +0530

    Update environment configuration to modify Pinecone and OpenRouter API keys, and add OpenAI API key for enhanced embedding generation capabilities. This change improves the management of API keys within the application.

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,11 +7,14 @@ export const ENV = {
   DB_NAME: "neondb",
   DB_USER: "neondb_owner",
   DB_PASSWORD: "npg_Ay86oaGNCKis",
-  PINECONE_API_KEY: "pcsk_2QbPND_M5a259ah1aQmz7VeejcnifPRQtPh9jC8T9erfPWpGuPcQHn2unxxjvi9bH32hY2",
+  PINECONE_API_KEY:
+    "pcsk_2QbPND_M5a259ah1aQmz7VeejcnifPRQtPh9jC8T9erfPWpGuPcQHn2unxxjvi9bH32hY2",
   PINECONE_INDEX_NAME: "semantic-search",
   REDIS_HOST: "localhost",
   REDIS_PORT: 6379,
-  OPENROUTER_API_KEY: "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",
+  OPENROUTER_API_KEY:
+    "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",
+  OPENAI_API_KEY:
+    "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",
   EMBEDDING_MODEL: "text-embedding-ada-002",
 } as const;
-
